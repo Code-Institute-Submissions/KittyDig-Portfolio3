d18a9678b717652f0e9b73c91661199ef681b41f
KittyDig <kittydignam@gmail.com>
2024-02-12 12:36:24 +0000

    Added game.py code to initialise the game

---
+++ game.py
@@ -0,0 +1,17 @@
+from board import Board
+from player import Player
+
+class BattleshipsGame:
+def __init__(self):
+# initialises the game
+self.player1 = Player("Player 1")
+self.player2 = Player("Player 2")
+pass
+
+def setup_game(self):
+# sets up the game, including board size and placing the ships
+pass
+
+def play(self):
+# main game loop
+pass
