4b32f369f053e0672b35240fb431926c853ce5e9
KittyDig <kittydignam@gmail.com>
2024-02-12 12:47:49 +0000

    Added run.py code to run the game

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# run.py
+from game import BattleshipsGame
+
+def main():
+    # creates an instance of the BattleshipsGame class
+    game = BattleshipsGame()
+
+    # sets up the game
+    game.setup_game()
+
+    # plays the game
+    game.play()
+
+if __name__ == "__main__":
+    main()
