36112a844cfdd0fad38fde4cd32fb8a3a5c7f87f
KittyDig <kittydignam@gmail.com>
2024-02-12 12:09:04 +0000

    Added base structure to board.py

---
+++ board.py
@@ -0,0 +1,20 @@
+class Board:
+def __init__():
+# initialises the game board
+pass
+
+def display():
+# displays the board
+pass
+
+def place_ship():
+# places a ship on the board
+pass
+
+def receive_attack():
+# processes an attack and update the board
+pass
+
+def all_ships_sunk():
+# checks if all ships are sunk
+pass
