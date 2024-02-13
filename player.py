a860fc7f858a74f6c3f8db341008ae0085dfde89
KittyDig <kittydignam@gmail.com>
2024-02-13 09:11:49 +0000

    Added objects attributes for the player.py file

--- player.py
+++ player.py
@@ -1,8 +1,8 @@
 class Player:
-def __init__():
-# initialises the player
-pass
+        def __init__(self, name):
+        # initialises the player
+        pass
 
-def make_guess():
-# gets the player's guess
-pass
+    def make_guess(self, opponent_board):
+        # gets the player's guess
+        pass
