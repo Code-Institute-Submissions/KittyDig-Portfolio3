fa3331fdf3a63cd06ed32d7ebb2ddb243bfb7231
KittyDig <kittydignam@gmail.com>
2024-02-13 09:27:12 +0000

    Added attributes to objects in board.py file

--- board.py
+++ board.py
@@ -1,20 +1,20 @@
 class Board:
-def __init__():
-# initialises the game board
-pass
+        def __init__(self, size):
+        # initialises the game board
+        pass
 
-def display():
-# displays the board
-pass
+    def display(self, show_ships=False):
+        # displays the board
+        pass
 
-def place_ship():
-# places a ship on the board
-pass
+    def place_ship(self, ship, row, col, orientation):
+        # places a ship on the board
+        pass
 
-def receive_attack():
-# processes an attack and update the board
-pass
+    def receive_attack(self, row, col):
+        # processes an attack and update the board
+        pass
 
-def all_ships_sunk():
-# checks if all ships are sunk
-pass
+    def all_ships_sunk(self):
+        # checks if all ships are sunk
+        pass
