0310e2f8b27ed49ff9c948e4fedc4592d0396fc7
KittyDig <kittydignam@gmail.com>
2024-02-16 11:30:43 +0000

    Added random import for ship placement, added logic for initialising the game board

--- board.py
+++ board.py
@@ -1,20 +1,25 @@
+# board.py
+import random
+
 class Board:
-        def __init__(self, size):
-        # initialises the game board
-        pass
+    def __init__(self, size):
+        ''' initialise the game board '''
+        self.size = size
+        self.grid = [[' ' for _ in range(size)] for _ in range(size)]  # empty grid
+        self.ships = []  # list to store ship coordinates
 
     def display(self, show_ships=False):
-        # displays the board
+        ''' displays the board '''
         pass
 
     def place_ship(self, ship, row, col, orientation):
-        # places a ship on the board
+        ''' places a ship on the board '''
         pass
 
     def receive_attack(self, row, col):
-        # processes an attack and update the board
+        ''' processes an attack and update the board '''
         pass
 
     def all_ships_sunk(self):
-        # checks if all ships are sunk
+        ''' checks if all ships are sunk '''
         pass
