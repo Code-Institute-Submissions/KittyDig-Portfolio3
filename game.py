5b7350a72a275d4578a339c50c827cd7e7c219cb
KittyDig <kittydignam@gmail.com>
2024-02-16 16:27:52 +0000

    Added logic to allow the first player to make their guess

--- game.py
+++ game.py
@@ -14,13 +14,17 @@ class BattleshipsGame:
 
     def setup_game(self):
         ''' sets up the game, including board size and placing the ships '''
-        for _ in range(2):
+        for _ in range(2): # places 2 ships on the board
             self.board1.place_ship(2, random.choice(['H', 'V']))
             self.board2.place_ship(2, random.choice(['H', 'V']))
 
     def play(self):
         ''' main game loop '''
         while not (self.board1.all_ships_sunk() or self.board2.all_ships_sunk()):
+            # player 1s turn
+            guess1 = self.player1.make_guess(self.board2)
+            result1 = self.board2.receive_attack(*guess1)
+            print(f"{self.player1.name} attacks {guess1} - Result: {result1}")
 
             if self.board2.all_ships_sunk():
                 break
