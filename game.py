4bd13382e0ae588d2700efe9a29414d7add1307a
KittyDig <kittydignam@gmail.com>
2024-02-19 14:38:43 +0000

    Board size reduced

--- game.py
+++ game.py
@@ -9,8 +9,8 @@ class BattleshipsGame:
         ''' initialises the game '''
         self.player1 = Player("Player 1")
         self.player2 = Player("Player 2")
-        self.board1 = Board(size=10)
-        self.board2 = Board(size=10)
+        self.board1 = Board(size=6)
+        self.board2 = Board(size=6)
 
     def setup_game(self):
         ''' sets up the game, including board size and placing the ships '''
