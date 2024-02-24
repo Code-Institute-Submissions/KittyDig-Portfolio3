e0495b17959e53cfab9e944176a8c381aba4083d
KittyDig <kittydignam@gmail.com>
2024-02-24 22:00:08 +0000

    Fixed code to not have any PEP8 errors

--- run.py
+++ run.py
@@ -1,16 +1,18 @@
 # run.py
 from game import BattleshipsGame
 
+
 def main():
     while True:
         try:
-            # logic for giving the player the option to choose between a computer controlled opponent or another person
+            # logic for giving the player the option to choose
+            # between a computer controlled opponent or another person
             print("Choose your opponent:")
             print("1. Play against a local player")
             print("2. Play against the computer")
 
             choice = int(input("Enter 1 or 2: "))
-                        
+
             if choice == 1:
                 player_type = "human"
             elif choice == 2:
@@ -27,8 +29,10 @@ def main():
         except ValueError:
             print("Invalid input. Please enter a number.")
 
+
 if __name__ == "__main__":
     main()
 
-# a much more basic version of def main() in this section was derived from code found on:
+# a much more basic version of def main() in
+# this section was derived from code found on:
 # https://codereview.stackexchange.com/questions/164697/game-of-battleship-2-player-python
